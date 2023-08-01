ed5f90158b4434e70e0170c114e0882b54b85872
Oleksandr Orlov <orlov0ae@gmail.com>
2023-08-01 23:50:54 +0300

    Lesson18

---
+++ Lesson18/Lesson18.java
@@ -0,0 +1,25 @@
+package Lesson18;
+
+import java.util.*;
+
+public class Lesson18 {
+
+    public static void main(String[] args) {
+
+        Set<Integer> hashSet = new HashSet<>();
+        Set<Integer> linkedHashSet = new LinkedHashSet<>();
+        Set<Integer> treeSet = new TreeSet<>();
+
+    }
+
+
+    public void setFill () {
+
+        Random random = new Random();
+        for (int i=0; i<=10; i++) {
+
+        }
+
+    }
+
+}
---
+++ Practice20/Practice20.java
@@ -0,0 +1,10 @@
+package Practice20;
+
+import java.util.Scanner;
+
+public class Practice20 {
+
+
+
+}
+
