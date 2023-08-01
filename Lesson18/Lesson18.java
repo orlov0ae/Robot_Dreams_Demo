5b64056328e14f387945b01cb9c5794319740594
Oleksandr Orlov <orlov0ae@gmail.com>
2023-08-02 00:36:05 +0300

    Lesson18

--- Lesson18/Lesson18.java
+++ Lesson18/Lesson18.java
@@ -1,25 +1,52 @@
 package Lesson18;
 
+
 import java.util.*;
+import java.util.HashSet;
 
 public class Lesson18 {
 
+    static Set<Integer> hashSet = new HashSet<>();
+    static Set<Integer> linkedHashset = new LinkedHashSet<>();
+    static Set<Integer> treeSet = new TreeSet<>();
+
     public static void main(String[] args) {
 
-        Set<Integer> hashSet = new HashSet<>();
-        Set<Integer> linkedHashSet = new LinkedHashSet<>();
-        Set<Integer> treeSet = new TreeSet<>();
+        add();
+        System.out.println("HashSet");
+        printHashSet(hashSet);
+        System.out.println("LinkedHashSet");
+        printHashSet(linkedHashset);
+        System.out.println("TreeSet");
+        printHashSet(treeSet);
 
     }
 
-
-    public void setFill () {
+    public static void add() {
 
         Random random = new Random();
-        for (int i=0; i<=10; i++) {
 
+        System.out.println("Порядок введених елементів");
+
+        for (int i=1; i<=10; i++) {
+            int elem = random.nextInt(50);
+            hashSet.add(elem);
+            linkedHashset.add(elem);
+            treeSet.add(elem);
+            System.out.println(elem);
         }
 
+        System.out.println("___________");
+
     }
+
+    public static void printHashSet(Set set) {
+
+        for (Object elem : set) {
+            System.out.println(elem);
+        }
+        System.out.println("_____________");
+    }
+
 
 }
