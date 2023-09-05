720e59ddf9f21173a4cbb0479aa7a7f349ab10ec
Oleksandr Orlov <orlov0ae@gmail.com>
2023-09-05 14:26:10 +0300

    Lesson29

---
+++ Lesson29/Lesson29.java
@@ -0,0 +1,32 @@
+package Lesson29;
+
+public class Lesson29 {
+
+    public static void main(String[] args) {
+
+        Thread thread1 = new Thread(() -> {
+            for(int i=1; i<=10; i++){
+                System.out.println("Thread #1. Number i=" + i);
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for(int j=1; j<=10; j++){
+                System.out.println("Thread #2. Number j=" + j);
+            }
+        });
+
+//        thread1.start();
+//        thread2.start();
+
+        runThreads(thread1, thread2);
+
+    }
+
+    private static void runThreads(Thread ... threads) {
+        for(Thread thread : threads) {
+            thread.start();
+        }
+    }
+
+}
